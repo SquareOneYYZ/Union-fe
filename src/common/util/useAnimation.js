123fce3cb1692eda0ecf6760558b1901067d3825
Aman marve <aman@ridesiq.com>
2026-05-19 16:03:32 +0530

    vitest changes added

---
+++ src/common/util/useAnimation.js
@@ -0,0 +1,13 @@
+  export const lerp = (a, b, t) => a + (b - a) * t;
+
+ export const easeInOutCubic = (t) => (t < 0.5 ? 4 * t * t * t : 1 - (-2 * t + 2) ** 3 / 2);
+
+  export const interpolateRotation = (start, end, p) => {
+    let diff = end - start;
+    if (diff > 180) diff -= 360;
+    if (diff < -180) diff += 360;
+    let r = start + diff * p;
+    if (r < 0) r += 360;
+    if (r >= 360) r -= 360;
+    return r;
+  };
